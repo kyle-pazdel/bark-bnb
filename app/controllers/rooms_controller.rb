35d99a69be272e5acb3a50e863c822e88dc9329a
Abraham V Rodriguez <pqavrr835@gmail.com>
2022-09-21 12:38:55 -0400

    Removes ability to show all rooms except reserved ones, just shows all rooms, removes admin and admin authentication

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,15 +1,14 @@
 class RoomsController < ApplicationController
   def index
-    if current_user && current_user.is_admin
-      @rooms = Room.all
-      render template: "rooms/index"
-    end
     @rooms = Room.all
-    @reservations = Reservation.all
-    @reservations.each do |reservation|
-      @rooms = @rooms.select { |room| room.id != reservation.room_id }
-    end
     render template: "rooms/index"
+
+    # @rooms = Room.all
+    # @reservations = Reservation.all
+    # @reservations.each do |reservation|
+    #   @rooms = @rooms.select { |room| room.id != reservation.room_id }
+    # end
+    # render template: "rooms/index"
   end
 
   def show
