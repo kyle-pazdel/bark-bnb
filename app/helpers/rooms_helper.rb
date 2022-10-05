66c376f6048dd9e257457192b808f9860b8750a0
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-10-05 14:04:18 -0700

    Adds styling for Review average with stars on rooms show page

--- app/helpers/rooms_helper.rb
+++ app/helpers/rooms_helper.rb
@@ -1,11 +1,14 @@
 module RoomsHelper
-
   def room_rating(room)
-    review = room.reviews
-    ratings = review.pluck(:rating)
-    total = ratings.sum
+    reviews = room.reviews
+    rating = reviews.pluck(:rating)
+    total = 0
+    rating.each do |num|
+      total += num.to_i
+    end
+    # total = rating.sum
     if total > 0
-      average = total / ratings.length
+      average = total / rating.length
       average = average.round(2)
     else
       average = "This room has no ratings."
@@ -15,7 +18,7 @@ module RoomsHelper
 
   def title_image_for(room, width = "", height = "")
     @image = room.room_images.first.url
-    return image_tag(@image, alt: "#{room.description} image one", width: width, height: height, class: "", style: "")
+    return image_tag(@image, alt: "#{room.description} image one", width: width, height: height)
   end
 
   def first_image_for(room, width = "", height = "")
